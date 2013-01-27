9df3ef5c0e318027070f0ebe0d4307eaa878e83d
Kiernan McGowan <timmcgowan625@gmail.com>
2013-01-26 23:21:05 -0600

    added basic example

---
+++ README.md
@@ -0,0 +1,6 @@
+cosmonaut
+---------
+
+Central package that needs to be installed to run a game server
+
+This will specifically server as the game server itself
---
+++ example/example-scene.js
@@ -0,0 +1,29 @@
+// example-scene.js
+// just and example of how to use cosmonaut
+
+var harness = require('cosmonaut-harness');
+
+var scene = new harness.scene();
+scene.attachToDom($('#context'));
+
+var sphere = new harness.primitive.sphere({
+  radius: 50
+});
+
+var light = new harness.light();
+
+var lambert = new harness.material();
+
+sphere.set('material', lambert);
+
+scene.addObject(sphere);
+scene.addLight(light);
+
+var time = 0;
+
+scene.on('prerender', function() {
+  time++;
+  sphere.scale(Math.cos(time/100), Math.sin(time/100), Math.cos(time/100) * Math.sin(time/100));
+});
+
+scene.startRenderLoop();
