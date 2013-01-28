bcf61dcc2e05f9d72cb84def7c15d431c8e2b95c
Kiernan McGowan <timmcgowan625@gmail.com>
2013-01-28 10:24:12 -0600

    updating examples

--- example/example-scene.js
+++ example/example-scene.js
@@ -3,11 +3,17 @@
 
 var harness = require('cosmonaut-harness');
 
-var scene = new harness.scene();
+var scene = new harness.scene({webgl: true});
 scene.attachToDom($('#context'));
 
 var sphere = new harness.primitive.sphere({
   radius: 50
+});
+
+var cube = new harness.primitive.cube({
+  x: -10,
+  y: 100,
+  z: 10
 });
 
 var light = new harness.light();
@@ -15,18 +21,19 @@ var light = new harness.light();
 var lambert = new harness.material();
 
 sphere.set('material', lambert);
+cube.set('material', lambert);
 
 scene.addObject(sphere);
+scene.addObject(cube);
+
 scene.addLight(light);
 
 var time = 0;
 
-scene.on('prerender', function() {
+/*scene.on('prerender', function() {
   time++;
-  sphere.scale(Math.cos(time/100), Math.sin(time/100), Math.cos(time/100) * Math.sin(time/100));
-  scene.camera.move(Math.cos(time/50), Math.sin(time/50), Math.cos(time/50) * Math.sin(time/50));
-});
+});*/
 
-scene.camera.set('lookAtObject', sphere);
+//scene.camera.set('lookAtObject', sphere);
 
 scene.startRenderLoop();
